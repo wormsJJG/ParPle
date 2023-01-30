2bd031bda1a40cd79cb3e216363fe53054795d43
Y00ujin <yujingim43@gmail.com>
2023-01-30 22:10:36 +0900

    feat: 프로젝트 생성

---
+++ ParPle/ParPle/ParPleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParPleApp.swift
+//  ParPle
+//
+//  Created by 김유진 on 2023/01/30.
+//
+
+import SwiftUI
+
+@main
+struct ParPleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
